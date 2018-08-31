e089cb11e8f97fab4698800e40b8dadf6a86303d
someThor <thor.erik.oyen@gmail.com>
2018-08-31 22:07:59 +0200

    Optional CoiniumServ branch select in .env file. Hacked jquery update.

--- coiniumserv/start.sh
+++ coiniumserv/start.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+BRANCH=master
+if [ -d "$INSTALL_PATH/CoiniumServ-develop/build/bin/Release" ]; then
+  BRANCH=develop
+fi
+
 INSTALL_PATH="/usr/src/app/"
 
-cd $INSTALL_PATH/CoiniumServ-master/build/bin/Release
+cd $INSTALL_PATH/CoiniumServ-${BRANCH}/build/bin/Release
+
+mkdir -p config
 if [ -d config ]; then
   if [ -d config/originals ]; then
     rm -rf config/originals
@@ -13,5 +20,5 @@ fi
 
 sleep 5
 cd $INSTALL_PATH
-mono ./CoiniumServ-master/build/bin/Release/CoiniumServ.exe
 export MONO_THREADS_PER_CPU=100
+mono ./CoiniumServ-${BRANCH}/build/bin/Release/CoiniumServ.exe
